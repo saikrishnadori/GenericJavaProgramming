6a9d6ba671401376809577f2e30c4975b305f2c0
Saikrishna <Saikrishna@LAPTOP-NOL6CDJ6>
2022-06-15 00:48:47 +0530

    added testG file

---
+++ journalDev/src/journalDev/Day1/AccessModifiers/pk2/TestG.java
@@ -0,0 +1,11 @@
+package journalDev.Day1.AccessModifiers.pk2;
+
+public class TestG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestE");
+
+	}
+
+}
